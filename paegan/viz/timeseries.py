c4547db60b410550550c2560fa804867a7b4facf
Alexander Crosby <acrosby@asascience.com>
2012-08-27 13:45:56 -0400

    created progressive vector timeseries plot tool

---
+++ paegan/viz/timeseries.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+
+def progressive_vectors(time, U, V, save=None, axes=None, **kwargs):
+    # Create figure and axes from matplotlib
+    if axes == None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+    else:
+        ax = axes
+    #print time.shape, U.shape, V.shape, np.zeros_like(U).shape
+    handle = ax.quiver(range(169), np.zeros_like(U), U, V,pivot='tail', **kwargs)
+    
+    if save == None:
+        pass
+    else:
+        fig.savefig(save, dpi=200)
+    return handle
+        
+    
