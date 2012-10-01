f48b7324fa9cbcfab6daa451529620d9762626f6
Kyle Wilcox <kwilcox@asascience.com>
2012-10-01 15:10:03 -0400

    Add a test file to local repository

--- tests/trajectory_test.py
+++ tests/trajectory_test.py
@@ -1,10 +1,4 @@
-import sys
-
-sys.path.append("/media/sf_Python/paegan")
-
-
 from paegan.viz.trajectory import CFTrajectory
 
-traj = CFTrajectory("/media/sf_Python/paegan/model_run_output.nc")
-
+traj = CFTrajectory("resources/trajectories.nc")
 traj.plot_animate('my_test_animation.mp4', view=(0,-90))
