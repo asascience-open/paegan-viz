6d894d7e23250cf1ea0f37bac7ad823a92ed0fd1
Kyle Wilcox <kwilcox@asascience.com>
2012-10-04 10:45:08 -0400

    Output .avi files in tests

--- tests/trajectory_test.py
+++ tests/trajectory_test.py
@@ -7,7 +7,7 @@ class TrajectoryTests(unittest.TestCase):
         
     def test_full_animation(self):
         traj = CFTrajectory(os.path.join(os.path.dirname(__file__), "resources/trajectories.nc"))
-        output_movie = os.path.join(os.path.dirname(__file__), "output/animation.mp4")
+        output_movie = os.path.join(os.path.dirname(__file__), "output/animation.avi")
         status = traj.plot_animate(output_movie, view=(0,-90), bathy="/home/dev/Development/paegan/paegan/resources/bathymetry/ETOPO1_Bed_g_gmt4.grd")
         assert status is False
         
@@ -16,6 +16,6 @@ class TrajectoryTests(unittest.TestCase):
         files = sorted(os.listdir(root_dir))
         files = map(lambda x: os.path.join(root_dir,x),files)
         #print files
-        output_movie = os.path.join(os.path.dirname(__file__), "output/traj.mp4")
+        output_movie = os.path.join(os.path.dirname(__file__), "output/traj.avi")
         status = save_animation(output_movie, files, 10, clear_temp=False)
         assert status is True
