a3652d1f4bff3402deb4d2fad0b8e25fdd0dd5c3
Kyle Wilcox <kwilcox@asascience.com>
2012-10-09 18:08:41 -0400

    Update traj test file with lots of timesteps.

--- tests/trajectory_test.py
+++ tests/trajectory_test.py
@@ -7,15 +7,15 @@ class TrajectoryTests(unittest.TestCase):
         
     def test_full_animation(self):
         traj = CFTrajectory(os.path.join(os.path.dirname(__file__), "resources/trajectories.nc"))
-        output_movie = os.path.join(os.path.dirname(__file__), "output/animation.avi")
-        status = traj.plot_animate(output_movie, view=(0,-90), bathy="/home/dev/Development/paegan/paegan/resources/bathymetry/ETOPO1_Bed_g_gmt4.grd")
-        assert status is False
+        output_movie = os.path.join(os.path.dirname(__file__), "output/from_netcdf.avi")
+        status = traj.plot_animate(output_movie, bathy="/home/dev/Development/paegan/paegan/resources/bathymetry/ETOPO1_Bed_g_gmt4.grd")
+        assert status is True
         
     def test_save_animation(self):
         root_dir = os.path.join(os.path.dirname(__file__), "resources/trajectory_images")
         files = sorted(os.listdir(root_dir))
         files = map(lambda x: os.path.join(root_dir,x),files)
         #print files
-        output_movie = os.path.join(os.path.dirname(__file__), "output/traj.avi")
-        status = save_animation(output_movie, files, 10, clear_temp=False)
+        output_movie = os.path.join(os.path.dirname(__file__), "output/from_files.avi")
+        status = save_animation(output_movie, files, clear_temp=False)
         assert status is True
